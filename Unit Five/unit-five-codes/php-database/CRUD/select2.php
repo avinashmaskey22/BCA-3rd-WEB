2ef1f5f08329d78ceee354254721820f46350c65
avinashmaskey22 <avinash.maskey@thamescollege.edu.np>
2024-06-12 22:25:32 +0545

    db topic codes added

---
+++ Unit Five/unit-five-codes/php-database/CRUD/select2.php
@@ -0,0 +1,32 @@
+<?php require_once 'db-connection.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Document</title>
+</head>
+
+<body>
+        <?php
+        $sql = "SELECT * FROM students";
+        $query = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($query) <= 0) {
+            echo "No data found in table.";
+        } else {
+            //fetch all data from a table at one go
+            // $data = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+            //fetch a data from a table one row at a time
+            // $row = mysqli_fetch_assoc($query);
+            $i = 1;
+            while ($row = mysqli_fetch_assoc($query)) {
+                echo $row['s_id'] . " " . $row['full_name'] . " " . $row['email'] . " " . $row['address'] . " " . $row['contact_number'] . "<br><br>";
+            }
+        }
+
+        ?>
+    </table>
+</body>
+
+</html>
